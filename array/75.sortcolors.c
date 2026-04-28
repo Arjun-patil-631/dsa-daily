b8fa1b79c476f46fa73c448476512dd6295d8d69
Arjun Patil <5631arjunpatil@gmail.com>
2026-04-28 19:35:40 +0530

    Create 75.sortcolors.c

---
+++ array/75.sortcolors.c
@@ -0,0 +1,16 @@
+void sortColors(int* nums, int numsSize) {
+    int count0, count1, count2, i;
+    count0=count1=count2=0;
+    for(i=0; i<numsSize; i++){
+        if(nums[i]==0) count0++;
+        else if(nums[i]==1) count1++;
+        else count2++;
+    }
+    for(i=0; i<count0; i++)
+        nums[i]=0;
+    for(i=count0; i<count0+count1; i++)
+        nums[i]=1;
+    for(i=count0+count1; i<count0+count1+count2; i++)
+        nums[i]=2;
+
+}
