5b233ab4030ed662bc9d09d9af1e03e2074f76e7
Arjun Patil <5631arjunpatil@gmail.com>
2026-05-06 06:55:29 +0530

    Add mergeTwoLists function for sorted lists
    Implemented a function to merge two sorted linked lists.

--- array/own_methods.c
+++ array/own_methods.c
@@ -15,3 +15,38 @@ void moveZeroes(int* nums, int numsSize) {
         }
     }
 }
+
+//merging two sorted lists
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+
+    struct ListNode dummy;
+    struct ListNode *temp = &dummy;
+
+    dummy.next = NULL;
+
+    while(list1 != NULL && list2 != NULL) {
+
+        if(list1->val <= list2->val) {
+            temp->next = list1;
+            list1 = list1->next;
+        }
+        else {
+            temp->next = list2;
+            list2 = list2->next;
+        }
+
+        temp = temp->next;
+    }
+
+    // attach remaining nodes
+    if(list1 != NULL)
+        temp->next = list1;
+    else
+        temp->next = list2;
+
+    return dummy.next;
+        
+    }
+};
