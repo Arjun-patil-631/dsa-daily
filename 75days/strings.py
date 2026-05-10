310c7de77216574f92ff33ed2af896434efa6f0f
Arjun Patil <5631arjunpatil@gmail.com>
2026-05-10 22:43:26 +0530

    Update strings.py
    10-07-2025

--- 75days/strings.py
+++ 75days/strings.py
@@ -16,3 +16,32 @@ class Solution(object):
                 
         return result
         
+#10-7-2025
+#longest common prefix in an array of strings
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        strs.sort()
+        first=strs[0]
+        last=strs[-1]
+        ans=[]
+        for i in range(min(len(first), len(last))):
+            if first[i]!=last[i]:
+                return ''.join(ans)
+            ans.append(first[i])
+        return ''.join(ans)
+
+#largest odd number in a string 
+class Solution(object):
+    def largestOddNumber(self, num):
+        ind=-1
+        i=0
+        for i in range(len(num)-1, -1, -1):
+            if (int(num[i])%2)==1:
+                ind=i
+                break
+        i=0
+        while i<=ind and num[i]==0:
+            i+=1
+        return num[i:ind+1]
