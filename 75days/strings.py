cf1b707b073f4f8ec83c69d89818d18cced5c9e2
Arjun Patil <5631arjunpatil@gmail.com>
2026-05-16 21:23:52 +0530

    Update strings.py
    16-05-2025

--- 75days/strings.py
+++ 75days/strings.py
@@ -109,3 +109,21 @@ class Solution(object):
                 res+=roman[s[i]]
         return res
         
+#16-05-2025
+#longest palindromic substring
+class Solution(object):
+    def longestPalindrome(self, s):
+        def expand(l,r):
+            while l>=0 and r<len(s) and s[l]==s[r]:
+                l-=1
+                r+=1
+            return s[l+1:r]
+        ans=""
+        for i in range(len(s)):
+            p1=expand(i,i)
+            p2=expand(i,i+1)
+            if len(p1)>len(ans):
+                ans=p1
+            if len(p2)>len(ans):
+                ans=p2
+        return ans
