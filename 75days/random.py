accae1cd05929ddd872845f5a884768c31d193eb
Arjun Patil <5631arjunpatil@gmail.com>
2026-05-17 22:59:20 +0530

    Update random.py
    17-05-2025

--- 75days/random.py
+++ 75days/random.py
@@ -25,3 +25,22 @@ class Solution(object):
         res.append(list(sset-fset))
         return res
     
+#revering a llinked list iteratively
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        prev = None
+        curr = head
+
+        while curr:
+            next_node = curr.next   # store next node
+            curr.next = prev        # reverse link
+            prev = curr             # move prev ahead
+            curr = next_node        # move curr ahead
+
+        return prev
+        
