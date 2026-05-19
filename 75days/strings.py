085e7832dbd583d16c682614b05697ba39a7f2ad
Arjun Patil <5631arjunpatil@gmail.com>
2026-05-19 23:11:25 +0530

    Update strings.py
    19-05-2025

--- 75days/strings.py
+++ 75days/strings.py
@@ -127,3 +127,17 @@ class Solution(object):
             if len(p2)>len(ans):
                 ans=p2
         return ans
+
+#longest substring without repeating characters
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        seen={}
+        left=0
+        max_len=0
+        for right in range(len(s)):
+            if s[right] in seen and seen[s[right]]>=left:
+                left=seen[s[right]]+1
+            
+            seen[s[right]]=right
+            max_len=max(max_len, right-left+1)
+        return max_len
