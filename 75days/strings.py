953be31636774ff35d3d1f9769e32c41901ac89d
Arjun Patil <5631arjunpatil@gmail.com>
2026-05-22 20:11:32 +0530

    Update strings.py

--- 75days/strings.py
+++ 75days/strings.py
@@ -164,5 +164,21 @@ class Solution(object):
             s.extend(str(count))
         chars[:len(s)]=s
         return len(s)
-                
+            
+#22-05-2026
+#valid palindrome
+class Solution(object):
+    def isPalindrome(self, s):
+        left=0
+        right=len(s)-1
+        while left<right:
+            while left<right and not s[left].isalnum():
+                left+=1
+            while left<right and not s[right].isalnum():
+                right-=1
+            if s[left].lower()!=s[right].lower():
+                return False
+            left+=1
+            right-=1
+        return True
             
