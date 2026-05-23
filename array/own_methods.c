a7c958ced6d4286264ccbb2d4e3f330ea8696568
Arjun Patil <5631arjunpatil@gmail.com>
2026-05-23 21:08:37 +0530

    Update own_methods.c
    23-05-2026

--- array/own_methods.c
+++ array/own_methods.c
@@ -50,3 +50,45 @@ public:
         
     }
 };
+
+//23-05-2026
+//add 2 numbers represented as linked lists
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* createNode(int val){
+    struct ListNode* newNode=(struct ListNode*)malloc(sizeof(struct ListNode));
+    newNode->val=val;
+    newNode->next=NULL;
+    return newNode;
+}
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode dummy;
+    dummy.next=NULL;
+
+    struct ListNode* temp= &dummy;
+    int carry=0;
+
+    while(l1!=NULL || l2!=NULL || carry!=0){
+        int sum=carry;
+
+        if(l1!=NULL){
+            sum+=l1->val;
+            l1=l1->next;
+        }
+        if(l2!=NULL){
+            sum+=l2->val;
+            l2=l2->next;
+        }
+        carry=sum/10;
+        struct ListNode* newNode=createNode(sum%10);
+        temp->next=newNode;
+        temp=temp->next;
+    }
+    return dummy.next;
+}
