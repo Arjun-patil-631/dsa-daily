9af68a7aa033919fbed4369e2971409b7c420b5c
Arjun Patil <5631arjunpatil@gmail.com>
2026-06-07 14:09:16 +0530

    Update strings.py
    07-07-2026

--- 75days/strings.py
+++ 75days/strings.py
@@ -255,4 +255,33 @@ class Solution(object):
             length+=1
             i-=1
         return length
+    
+#07-06-2026
+#string to integer(atoi)
+INT_MIN=-2**31
+INT_MAX=2**31-1
+def helper(s,i,num,sign):
+    if i>=len(s) or not s[i].isdigit():
+        return sign*num
+    num=num*10+int(s[i])
+
+    if sign*num>=INT_MAX: 
+        return INT_MAX
+    if sign*num<=INT_MIN: 
+        return INT_MIN
+
+    return helper(s,i+1,num, sign)
+
+class Solution(object):
+    def myAtoi(self, s):
+        i=0
+        while i<len(s) and s[i]==' ':
+            i+=1
+        sign = 1
+        if i < len(s) and (s[i] == '+' or s[i] == '-'):
+            sign = -1 if s[i] == '-' else 1
+            i += 1
+
+        return helper(s, i, 0, sign)
+        
             
