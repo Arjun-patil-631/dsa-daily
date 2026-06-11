0380348d4bebd7f17f2494837eb4ba0d5126251d
Arjun Patil <5631arjunpatil@gmail.com>
2026-06-11 19:40:22 +0530

    Create topics.py
    11-06-2026

---
+++ 75days/topics.py
@@ -0,0 +1,38 @@
+#TREES
+#11-06-2026
+#preorder traversal of a binary tree
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        arr = []
+        def preorder(node):
+            if not node:
+                return
+            arr.append(node.val)
+            preorder(node.left)
+            preorder(node.right)
+        preorder(root)
+        return arr
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        result=[]
+        def postorder(node):
+            if not node:
+                return 
+            postorder(node.left)
+            postorder(node.right)
+            result.append(node.val)
+        postorder(root)
+        return result
