5a88947b4004fa7bcb6f861e676b216e73f10974
Arjun Patil <5631arjunpatil@gmail.com>
2026-06-14 13:42:46 +0530

    Update topics.py
    14-06-2026

--- 75days/topics.py
+++ 75days/topics.py
@@ -62,3 +62,36 @@ class MinStack(object):
 
     def getMin(self):
         return self.minstack[-1]
+
+#linked list
+#14-06-2026
+#rotate list
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def rotateRight(self, head, k):
+        if not head or not head.next or k==0:
+            return head
+        n=1
+        tail=head
+        while tail.next:
+            tail=tail.next
+            n+=1
+        k %=n
+        if k==0:
+            return head
+        #make it circular list
+        tail.next=head
+
+        steps=n-k-1
+        new_tail=head
+
+        for _ in range(steps):
+            new_tail=new_tail.next
+        new_head=new_tail.next
+        new_tail.next=None
+
+        return new_head
