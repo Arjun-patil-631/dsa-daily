7e77fa2e274dc4d50dbcb3e08c77c8493932b888
Arjun Patil <5631arjunpatil@gmail.com>
2026-06-18 21:54:17 +0530

    Update topics.py
    18-06-2026

--- 75days/topics.py
+++ 75days/topics.py
@@ -212,3 +212,14 @@ class Solution(object):
                     return ch
 
         return '.'
+
+#18-06-2026
+#mirror image
+class Solution(object):
+    def rotate(self, matrix):
+        n=len(matrix)
+        for i in range(n):
+            for j in range(i+1, n):
+                matrix[i][j], matrix[j][i]=matrix[j][i], matrix[i][j]
+        for i in range(n):
+            matrix[i].reverse()
