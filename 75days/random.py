9b297b8d284397ad6f1bf0dac8f179979f7f7e74
Arjun Patil <5631arjunpatil@gmail.com>
2026-06-27 20:35:24 +0530

    Update random.py
    27-06-2026

--- 75days/random.py
+++ 75days/random.py
@@ -240,5 +240,17 @@ class Solution(object):
             else:
                 high-=1
         return nums[low]
+    
+#27-06-2026
+#climbing stairs
+class Solution(object):
+    def climbStairs(self, n):
+        if n<=2:
+            return n
+        first=1
+        second=2
+        for i in range(3,n+1):
+            first, second= second, first+second
+        return second
 
                
