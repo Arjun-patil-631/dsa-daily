a37f266e9e522cac13898b5442131e9de750ea7a
Arjun Patil <5631arjunpatil@gmail.com>
2026-07-05 18:15:21 +0530

    Create hash.py

---
+++ 75days/hash.py
@@ -0,0 +1,37 @@
+4-7-2026
+#linked list cycle 2
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        slow=fast=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+            if slow==fast:
+                break
+        else:
+            return None
+        slow=head
+        while slow!=fast:
+            slow=slow.next
+            fast=fast.next
+        return slow
+
+#5-07-2026
+#isomorphic strings
+
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        m1, m2=[0] * 256, [0] * 256
+        n=len(s)
+        for i in range(n):
+            if m1[ord(s[i])]!=m2[ord(t[i])]:
+                return False
+            m1[ord(s[i])]=i+1
+            m2[ord(t[i])]=i+1
+        return True
