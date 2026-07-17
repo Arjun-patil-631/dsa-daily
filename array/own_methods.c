f53d4d7fac95908f3dd31c33e9a7b39e5f5a51e1
Arjun-patil-631 <5631arjunpatil@gmail.com>
2026-07-17 22:13:10 +0530

    Update own_methods.c
    17-07-2026

--- array/own_methods.c
+++ array/own_methods.c
@@ -104,3 +104,25 @@ int numOfStrings(char** patterns, int patternsSize, char* word) {
     }
     return count;
 }
+
+//17-07-2026
+//reverse a linked list
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* prev=NULL;
+    struct ListNode* curr=head;
+    struct ListNode* next=NULL;
+    while(curr!=NULL){
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+    }
+    return prev;
+}
