49e6f30ea782a8de5c028d5d6dea3bd49c4db124
Arjun-patil-631 <5631arjunpatil@gmail.com>
2026-07-21 20:09:30 +0530

    21-07

--- 75days/topics.py
+++ 75days/topics.py
@@ -382,3 +382,19 @@ class Solution(object):
 
         return concat * digit_sum
 
+#21-07-2026
+#color sort
+class Solution(object):
+    def sortColors(self, nums):
+        low=mid=0
+        high=len(nums)-1
+        while mid<=high:
+            if nums[mid]==0:
+                nums[low], nums[mid]=nums[mid], nums[low]
+                low+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid], nums[high]=nums[high], nums[mid]
+                high-=1
