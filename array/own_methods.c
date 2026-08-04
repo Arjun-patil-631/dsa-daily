4d202234c11e71a39d35f38f675bb689dda8e68e
Arjun-patil-631 <5631arjunpatil@gmail.com>
2026-08-04 22:54:15 +0530

    04-08

--- array/own_methods.c
+++ array/own_methods.c
@@ -126,3 +126,23 @@ struct ListNode* reverseList(struct ListNode* head) {
     }
     return prev;
 }
+
+//04-08-2026
+int maxProduct(int n) {
+    int first = 0, second = 0;
+
+    while (n > 0) {
+        int digit = n % 10;
+
+        if (digit >= first) {
+            second = first;
+            first = digit;
+        } else if (digit > second) {
+            second = digit;
+        }
+
+        n /= 10;
+    }
+
+    return first * second;
+}
