ef668e18d52b564ae1eafb88bf2c489c019f2d02
Arjun-patil-631 <5631arjunpatil@gmail.com>
2026-08-11 21:08:52 +0530

    //11-08-2026 // Smallest Missing Integer Greater Than Sequential Prefix Sum

--- injava/my.java
+++ injava/my.java
@@ -55,3 +55,33 @@ class Solution {
         return i;
     }
 }
+
+//11-08-2026
+// Smallest Missing Integer Greater Than Sequential Prefix Sum
+class Solution {
+    public int missingInteger(int[] nums) {
+        int total=nums[0];
+
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]==nums[i-1]+1){
+                total+=nums[i];
+            }
+            else{
+                break;
+            }
+        }
+        //store all numbers in a hash set
+        HashSet<Integer>set=new HashSet<>();
+
+        for(int num : nums){
+            set.add(num);
+        }
+
+        //find the smallest missing integer
+        while(set.contains(total)){
+            total++;
+        }
+
+        return total;
+    }
+}
