fd7659aae302785f1a70d2cc60c514c446163a9a
Arjun-patil-631 <5631arjunpatil@gmail.com>
2026-08-20 23:51:22 +0530

    Distribute Elements Into Two Arrays I

--- array/own_methods.c
+++ array/own_methods.c
@@ -241,3 +241,43 @@ int arrayPairSum(int* nums, int numsSize)
 
     return sum;
 }
+
+//20-08-2026
+//Distribute Elements Into Two Arrays I
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* resultArray(int* nums, int numsSize, int* returnSize) {
+    int *arr1 = malloc(numsSize * sizeof(int));
+    int *arr2 = malloc(numsSize * sizeof(int));
+    int *ans = malloc(numsSize * sizeof(int));
+
+    int n1 = 0, n2 = 0;
+
+    arr1[n1++] = nums[0];
+    arr2[n2++] = nums[1];
+
+    for (int i = 2; i < numsSize; i++) {
+        if (arr1[n1 - 1] > arr2[n2 - 1]) {
+            arr1[n1++] = nums[i];
+        } else {
+            arr2[n2++] = nums[i];
+        }
+    }
+
+    int k = 0;
+
+    for (int i = 0; i < n1; i++) {
+        ans[k++] = arr1[i];
+    }
+
+    for (int i = 0; i < n2; i++) {
+        ans[k++] = arr2[i];
+    }
+
+    free(arr1);
+    free(arr2);
+
+    *returnSize = numsSize;
+    return ans;
+}
