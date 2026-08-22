8ad4aa895b29641b5210a3d58f797bc03c1095f2
Arjun-patil-631 <5631arjunpatil@gmail.com>
2026-08-22 23:20:52 +0530

    Check Divisibility by Digit Sum and Product

--- injava/my.java
+++ injava/my.java
@@ -85,3 +85,24 @@ class Solution {
         return total;
     }
 }
+
+// daily question 
+// Check Divisibility by Digit Sum and Product
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int original = n;
+        int sum = 0;
+        int product = 1;
+
+        while (n > 0) {
+            int digit = n % 10;
+
+            sum += digit;
+            product *= digit;
+
+            n /= 10;
+        }
+
+        return original % (sum + product) == 0;
+    }
+}
