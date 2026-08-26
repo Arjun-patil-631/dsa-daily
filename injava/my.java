35462c4de106018c8f8e9d2796af4f8235c3392a
Arjun-patil-631 <5631arjunpatil@gmail.com>
2026-08-26 23:56:37 +0530

    26-08 remove duplicates form sorted list II

--- injava/my.java
+++ injava/my.java
@@ -106,3 +106,38 @@ class Solution {
         return original % (sum + product) == 0;
     }
 }
+
+//Remove Duplicates from Sorted List II
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+        ListNode curr = head;
+
+        while (curr != null) {
+
+            // Duplicate found
+            if (curr.next != null && curr.val == curr.next.val) {
+
+                int value = curr.val;
+
+                // Skip all nodes with the same value
+                while (curr != null && curr.val == value) {
+                    curr = curr.next;
+                }
+
+                prev.next = curr;
+            }
+            else {
+                // Current node is unique
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
