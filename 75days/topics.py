79d344bb0e6d1ab26cdd4682ef759446643862c7
Arjun-patil-631 <5631arjunpatil@gmail.com>
2026-08-27 23:46:32 +0530

    threeSum

--- 75days/topics.py
+++ 75days/topics.py
@@ -445,3 +445,32 @@ class Solution(object):
             if close<open:
                 backtrack(s+")",open, close+1)
         backtrack("",0,0)
+
+#27-08
+#threeSum
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        n=len(nums)
+        res=[]
+        for i in range(n):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            j=i+1
+            k=n-1
+            while j<k:
+                ans=nums[i]+nums[j]+nums[k]
+                if ans==0:
+                    res.append([nums[i],nums[j],nums[k]])
+                    j+=1
+                    k-=1
+                    while j<k and nums[j]==nums[j-1]:
+                        j+=1
+                    while j<k and nums[k]==nums[k+1]:
+                        k-=1
+                elif ans>0:
+                    k-=1
+                else:
+                    j+=1
+        return res
+        
