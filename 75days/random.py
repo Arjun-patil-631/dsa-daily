59e1ee9fc0fcd83211773688f3a45e8d74fc37da
Arjun-patil-631 <5631arjunpatil@gmail.com>
2026-08-29 22:34:43 +0530

    maximum depth of binary tree

--- 75days/random.py
+++ 75days/random.py
@@ -427,5 +427,21 @@ class Solution(object):
             curr=curr.next
         return dummy.next
 
-        
+#29-08-2026
+#maximum depth of binary tree
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root):
+        if root is None:
+            return 0
+
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+
+        return 1 + max(left, right)
         
