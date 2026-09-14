d4961bbcfa8930eebfd7de84859b4cc6027e296b
Arjun-patil-631 <5631arjunpatil@gmail.com>
2026-09-14 22:29:12 +0530

    rectangle overlap

--- 75days/random.py
+++ 75days/random.py
@@ -445,3 +445,14 @@ class Solution:
 
         return 1 + max(left, right)
         
+#daily question
+#rectangale overlap
+class Solution:
+    def isRectangleOverlap(self, rec1, rec2):
+        if rec1[2] <= rec2[0] or rec2[2] <= rec1[0]:
+            return False
+
+        if rec1[3] <= rec2[1] or rec2[3] <= rec1[1]:
+            return False
+
+        return True
