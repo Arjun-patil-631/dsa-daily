f1f75270c405e309ff3375c727d4696161d8914e
Arjun-patil-631 <5631arjunpatil@gmail.com>
2026-09-23 23:08:36 +0530

    added one more method to solve the problem

--- binary/search/simple.py
+++ binary/search/simple.py
@@ -12,3 +12,11 @@ class Solution(object):
             else:
                 high=mid-1
         return low
+#now lets go into depth
+class Solution(object):
+    def missingNumber(self, nums):
+        n=len(nums)
+        expected_sum=n*(n+1)//2
+        actual_sum=sum(nums)
+
+        return expected_sum-actual_sum
