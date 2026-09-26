f984b3745bfc6554a89c7da00fc8856de9a65f95
Arjun-patil-631 <5631arjunpatil@gmail.com>
2026-09-26 22:11:58 +0530

    single number II

--- array/leetcode.py
+++ array/leetcode.py
@@ -439,3 +439,17 @@ class Solution:
  
         backtrack()
         return ans
+
+#single number II
+class Solution(object):
+    def singleNumber(self, nums):
+        count={}
+
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+
+        for num in nums:
+            if count[num] == 1:
+                return num
+
+        
